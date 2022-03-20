0ed098437f76a8a5bb196fec989eedebf33c7eaf
MRonzio <mirko.ronzio@gmail.com>
2022-03-20 14:56:06 +0100

    added pscanchip script for a single matrix mode

---
+++ run_pscanchip_singlemat.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# this script needs to be run from the folder where input resides.
+# it runs pscanchip software on chip-seq summits peaks (or symmetrically extended from summits).
+# the resulted bed.ris file will be moved to the "pscanchip" folder whithin the same directory, 
+# which will  be created if not present.
+# genome: hg19 or mm10
+# matrix: any jaspar matrix (e.g. MA0060.1)
+
+# usage
+# ./run_pscanchip_singlemat.sh genome matrix file1.bed file2.bed .... fileN.bed
+
+pscanchip_folder=$(dirname $(readlink -f $(which pscan_chip)))  # or set the absolute path of pscanchip folder
+genome=$pscanchip_folder/$1
+matrices=$pscanchip_folder/jaspar_2022_R.wil
+matrix=$2
+
+mkdir -p pscanchip
+for f in ${@:3}
+do
+pscan_chip -r $f -g $genome -M $matrices -m $matrix
+mv  ${f}.res pscanchip/
+done
+
+
+
+
+
+
+
+
