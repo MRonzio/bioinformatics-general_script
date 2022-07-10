2c9e6ca217c9e38e51ef66422191651a2b22d1e8
RonzioM <mirko.ronzio@gmail.com>
2022-07-10 18:24:35 +0200

    fix return fastq file list

--- download_sra_explorer_fastq.sh
+++ download_sra_explorer_fastq.sh
@@ -14,15 +14,21 @@ SRAFILE=$1
 
 echo "reading file" $SRAFILE
 
+# print list of file names
+echo "saving list of sra files to SRA_download_filenames.txt.."
+awk -F "-o" '{print $2}' $SRAFILE > SRA_download_filenames.txt
+
+
+# editing download commands
+echo "editing download commands.."
+echo "saving download commands to download_script.sh.."
 sed '1 ! s/$/ -C -/' $SRAFILE > download_script.sh
 
 
 # first time
-
 bash download_script.sh
 
 # retry if any problem
-
 bash download_script.sh
 
 
@@ -31,6 +37,4 @@ rm download_script.sh
 
 
 echo "done"
-
-
 
